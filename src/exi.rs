eee724807963374500ed5998504b9e12bb5a8ad3
novenary <novenary@kwak.zip>
2023-11-25 18:19:53 +0200

    Implement async command monitor

--- src/exi.rs
+++ src/exi.rs
@@ -55,9 +55,11 @@ pub(crate) struct Exi<
     CS: pio::StateMachineIndex,
     RX: pio::StateMachineIndex,
     TX: pio::StateMachineIndex,
+    CMD: pio::StateMachineIndex,
     CHI: dma::ChannelIndex,
     const PIO_IRQ: usize,
     const CS_IRQ: u8,
+    const CMD_IRQ: u8,
 > {
     /// The chip-select monitor
     ///
@@ -70,12 +72,15 @@ pub(crate) struct Exi<
     /// The data transmit state machine
     sm_tx: PioStateMachine<P, TX>,
 
+    /// The command monitor
+    sm_cmd: PioStateMachine<P, CMD>,
+
     /// A single DMA channel ought to be enough for anybody
     dma: Option<dma::Channel<CHI>>,
 }
 
 /// ZST to encapsulate the ISR implementation
-pub(crate) struct ExiIntHandler<P: pio::PIOExt, const PIO_IRQ: usize, const CS_IRQ: u8> {
+pub(crate) struct PioIntHandler<P: pio::PIOExt, const PIO_IRQ: usize, const CS_IRQ: u8> {
     _pio: marker::PhantomData<P>,
 }
 
@@ -87,13 +92,14 @@ pub(crate) struct ExiPins<CS: gpio::AnyPin, CLK: gpio::AnyPin, DO: gpio::AnyPin,
     pub di: DI,
 }
 
-impl<P, CS, RX, TX, CHI, const PIO_IRQ: usize, const CS_IRQ: u8>
-    Exi<P, CS, RX, TX, CHI, PIO_IRQ, CS_IRQ>
+impl<P, CS, RX, TX, CMD, CHI, const PIO_IRQ: usize, const CS_IRQ: u8, const CMD_IRQ: u8>
+    Exi<P, CS, RX, TX, CMD, CHI, PIO_IRQ, CS_IRQ, CMD_IRQ>
 where
     P: pio::PIOExt,
     CS: pio::StateMachineIndex,
     RX: pio::StateMachineIndex,
     TX: pio::StateMachineIndex,
+    CMD: pio::StateMachineIndex,
     CHI: dma::ChannelIndex,
 {
     /// Initialize EXI with all the resources it requires
@@ -104,10 +110,15 @@ where
             pio::UninitStateMachine<(P, CS)>,
             pio::UninitStateMachine<(P, RX)>,
             pio::UninitStateMachine<(P, TX)>,
+            pio::UninitStateMachine<(P, CMD)>,
         ),
         pins: ExiPins<PinCs, PinClk, PinDo, PinDi>,
         dma: dma::Channel<CHI>,
-    ) -> (Self, ExiIntHandler<P, PIO_IRQ, CS_IRQ>)
+    ) -> (
+        Self,
+        PioIntHandler<P, PIO_IRQ, CS_IRQ>,
+        PioIntHandler<P, PIO_IRQ, CMD_IRQ>,
+    )
     where
         PinCs: gpio::AnyPin,
         PinCs::Id: gpio::ValidFunction<P::PinFunction>,
@@ -129,19 +140,24 @@ where
         // Required to outdrive RTC-DOL
         pin_di.set_drive_strength(gpio::OutputDriveStrength::EightMilliAmps);
 
-        let (sm_cs, sm_rx, sm_tx) = state_machines;
+        let (sm_cs, sm_rx, sm_tx, sm_cmd) = state_machines;
         let sm_cs = init_sm_cs(pio, sm_cs, CS_IRQ, &pin_cs, &pin_di);
         let sm_rx = init_sm_rx(pio, sm_rx, &pin_cs, &pin_clk, &pin_do);
         let sm_tx = init_sm_tx(pio, sm_tx, &pin_clk, &pin_di);
+        let sm_cmd = init_sm_cmd(pio, sm_cmd, CMD_IRQ, &pin_cs, &pin_clk, &pin_do);
 
         (
             Self {
                 sm_cs,
                 sm_rx,
                 sm_tx,
+                sm_cmd,
                 dma: Some(dma),
             },
-            ExiIntHandler {
+            PioIntHandler {
+                _pio: marker::PhantomData {},
+            },
+            PioIntHandler {
                 _pio: marker::PhantomData {},
             },
         )
@@ -165,21 +181,52 @@ where
         PIO: rtic::Mutex<T = pio::PIO<P>>,
         W: rtic::Mutex<T = Option<task::Waker>>,
     {
+        self.wait_irq(pio, cs_waker, CS_IRQ).await;
+    }
+
+    /// Wait for IRQ to fire (level-triggered)
+    async fn wait_irq<PIO, W>(&self, pio: &mut PIO, waker: &mut W, irq: u8)
+    where
+        PIO: rtic::Mutex<T = pio::PIO<P>>,
+        W: rtic::Mutex<T = Option<task::Waker>>,
+    {
         future::poll_fn(|poll_ctx| {
             pio.lock(|pio| {
-                if pio.get_irq_raw() & (1 << CS_IRQ) != 0 {
-                    pio.clear_irq(1 << CS_IRQ);
+                if pio.get_irq_raw() & (1 << irq) != 0 {
+                    pio.clear_irq(1 << irq);
                     return core::task::Poll::Ready(());
                 }
 
-                cs_waker.lock(|w| {
+                waker.lock(|w| {
                     w.replace(poll_ctx.waker().clone());
                 });
-                pio.irq::<PIO_IRQ>().enable_sm_interrupt(CS_IRQ);
+                pio.irq::<PIO_IRQ>().enable_sm_interrupt(irq);
                 core::task::Poll::Pending
             })
         })
         .await;
+    }
+
+    /// Receive a command
+    pub(crate) async fn receive_command<PIO, W>(
+        &mut self,
+        pio: &mut PIO,
+        cmd_waker: &mut W,
+    ) -> Option<u32>
+    where
+        PIO: rtic::Mutex<T = pio::PIO<P>>,
+        W: rtic::Mutex<T = Option<task::Waker>>,
+    {
+        pio.lock(|pio| {
+            pio.clear_irq(1 << CMD_IRQ);
+        });
+        self.wait_irq(pio, cmd_waker, CMD_IRQ).await;
+
+        let mut rx = self.sm_cmd.rx.take().unwrap();
+        let res = rx.read();
+        self.sm_cmd.rx.replace(rx);
+
+        res
     }
 
     /// Respond to a command
@@ -226,16 +273,16 @@ where
     }
 }
 
-impl<P, const PIO_IRQ: usize, const CS_IRQ: u8> ExiIntHandler<P, PIO_IRQ, CS_IRQ>
+impl<P, const PIO_IRQ: usize, const IRQ_LINE: u8> PioIntHandler<P, PIO_IRQ, IRQ_LINE>
 where
     P: pio::PIOExt,
 {
     /// The real ISR implementation
-    pub(crate) fn on_pio_interrupt(&self, pio: &pio::PIO<P>, cs_waker: &mut Option<task::Waker>) {
+    pub(crate) fn on_irq(&self, pio: &pio::PIO<P>, waker: &mut Option<task::Waker>) {
         let irq = pio.irq::<PIO_IRQ>();
-        if pio.get_irq_raw() & (1 << CS_IRQ) != 0 {
-            irq.disable_sm_interrupt(CS_IRQ);
-            if let Some(w) = cs_waker.take() {
+        if pio.get_irq_raw() & (1 << IRQ_LINE) != 0 {
+            irq.disable_sm_interrupt(IRQ_LINE);
+            if let Some(w) = waker.take() {
                 w.wake();
             }
         }
@@ -400,6 +447,62 @@ where
     let (sm, rx, tx) = pio::PIOBuilder::from_program(installed)
         .out_pins(pin_di, 1)
         .autopull(true)
+        .build(sm);
+    let sm = sm.start();
+
+    PioStateMachine {
+        sm,
+        rx: Some(rx),
+        tx: Some(tx),
+    }
+}
+
+/// The command monitor
+fn init_sm_cmd<P, SMI, PinCs, PinClk, PinDo>(
+    pio: &mut pio::PIO<P>,
+    sm: pio::UninitStateMachine<(P, SMI)>,
+    irq_cmd: u8,
+    pin_cs: &PinCs,
+    pin_clk: &PinClk,
+    pin_do: &PinDo,
+) -> PioStateMachine<P, SMI>
+where
+    P: pio::PIOExt,
+    SMI: pio::StateMachineIndex,
+    PinCs: gpio::AnyPin<Function = P::PinFunction>,
+    PinClk: gpio::AnyPin<Function = P::PinFunction>,
+    PinDo: gpio::AnyPin<Function = P::PinFunction>,
+{
+    let pin_cs = pin_cs.borrow().id().num;
+    let pin_clk = pin_clk.borrow().id().num;
+    let pin_do = pin_do.borrow().id().num;
+
+    let mut a = ::pio::Assembler::<{ ::pio::RP2040_MAX_PROGRAM_SIZE }>::new();
+    let mut wrap_source = a.label();
+    let mut wrap_target = a.label();
+    let mut bit_loop = a.label();
+    {
+        use ::pio::*;
+        a.bind(&mut wrap_target);
+        a.wait(1, WaitSource::GPIO, pin_cs, false);
+        a.wait(0, WaitSource::GPIO, pin_cs, false);
+
+        a.set(SetDestination::X, 32 - 1); // jmp compares prior to decrement
+        a.bind(&mut bit_loop);
+        a.wait(0, WaitSource::GPIO, pin_clk, false);
+        a.wait(1, WaitSource::GPIO, pin_clk, false);
+        a.r#in(InSource::PINS, 1);
+        a.jmp(JmpCondition::XDecNonZero, &mut bit_loop);
+
+        a.irq(false, false, irq_cmd, false);
+        a.bind(&mut wrap_source);
+    }
+    let program = a.assemble_with_wrap(wrap_source, wrap_target);
+
+    let installed = pio.install(&program).unwrap();
+    let (sm, rx, tx) = pio::PIOBuilder::from_program(installed)
+        .in_pin_base(pin_do)
+        .autopush(true)
         .build(sm);
     let sm = sm.start();
 
--- src/main.rs
+++ src/main.rs
@@ -50,12 +50,14 @@ mod app {
     struct Shared {
         pio0: pio::PIO<pac::PIO0>,
         cs_waker: Option<core::task::Waker>,
+        cmd_waker: Option<core::task::Waker>,
     }
 
     #[local]
     struct Local {
-        exi: exi::Exi<pac::PIO0, pio::SM0, pio::SM1, pio::SM2, dma::CH0, 0, 0>,
-        exi_int: exi::ExiIntHandler<pac::PIO0, 0, 0>,
+        exi: exi::Exi<pac::PIO0, pio::SM0, pio::SM1, pio::SM2, pio::SM3, dma::CH0, 0, 0, 1>,
+        cs_int: exi::PioIntHandler<pac::PIO0, 0, 0>,
+        cmd_int: exi::PioIntHandler<pac::PIO0, 0, 1>,
         payload: &'static [u32],
     }
 
@@ -85,7 +87,7 @@ mod app {
 
         let dma = ctx.device.DMA.split(&mut ctx.device.RESETS);
 
-        let (mut pio0, pio0_sm0, pio0_sm1, pio0_sm2, _pio0_sm3) =
+        let (mut pio0, pio0_sm0, pio0_sm1, pio0_sm2, pio0_sm3) =
             ctx.device.PIO0.split(&mut ctx.device.RESETS);
 
         #[cfg(not(feature = "pbcompat"))]
@@ -104,8 +106,12 @@ mod app {
             di: pins.gpio6,
         };
 
-        let (exi, exi_int) =
-            exi::Exi::new(&mut pio0, (pio0_sm0, pio0_sm1, pio0_sm2), exi_pins, dma.ch0);
+        let (exi, cs_int, cmd_int) = exi::Exi::new(
+            &mut pio0,
+            (pio0_sm0, pio0_sm1, pio0_sm2, pio0_sm3),
+            exi_pins,
+            dma.ch0,
+        );
 
         let payload = crate::check_payload();
 
@@ -115,23 +121,31 @@ mod app {
             Shared {
                 pio0,
                 cs_waker: None,
+                cmd_waker: None,
             },
             Local {
                 exi,
-                exi_int,
+                cs_int,
+                cmd_int,
                 payload,
             },
         )
     }
 
-    #[task(local = [exi, payload], shared = [pio0, cs_waker])]
+    #[task(local = [exi, payload], shared = [pio0, cs_waker, cmd_waker])]
     async fn root(ctx: root::Context) {
         let root::SharedResources {
             mut pio0,
             mut cs_waker,
+            mut cmd_waker,
             ..
         } = ctx.shared;
         let root::LocalResources { exi, payload, .. } = ctx.local;
+
+        loop {
+            let cmd = exi.receive_command(&mut pio0, &mut cmd_waker).await;
+            defmt::debug!("{:x}", cmd);
+        }
 
         exi.bus_idle(&mut pio0, &mut cs_waker).await;
         loop {
@@ -140,13 +154,25 @@ mod app {
         }
     }
 
-    #[task(binds = PIO0_IRQ_0, local = [exi_int], shared = [pio0, cs_waker])]
+    #[task(
+        binds = PIO0_IRQ_0,
+        local = [cs_int, cmd_int],
+        shared = [pio0, cs_waker, cmd_waker],
+    )]
     fn pio0_irq0(ctx: pio0_irq0::Context) {
-        let pio0_irq0::SharedResources { pio0, cs_waker, .. } = ctx.shared;
-        let pio0_irq0::LocalResources { exi_int, .. } = ctx.local;
+        let pio0_irq0::SharedResources {
+            pio0,
+            cs_waker,
+            cmd_waker,
+            ..
+        } = ctx.shared;
+        let pio0_irq0::LocalResources {
+            cs_int, cmd_int, ..
+        } = ctx.local;
 
-        (pio0, cs_waker).lock(|pio, cs_waker| {
-            exi_int.on_pio_interrupt(pio, cs_waker);
+        (pio0, cs_waker, cmd_waker).lock(|pio, cs_waker, cmd_waker| {
+            cs_int.on_irq(pio, cs_waker);
+            cmd_int.on_irq(pio, cmd_waker);
         });
     }
 }
